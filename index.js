b7d6968263f3faacbf6b45a8d5ca9727f0dbdd93
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-03 09:01:22 +0300

    Modifying the dom

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main=document.querySelector('#main');
+main.remove();
+const newHeader=document.createElement('h1');
+newHeader.setAttribute('id','victory');
+document.body.appendChild(newHeader);
+newHeader.innerHTML="YOUR-NAME is the champion";
